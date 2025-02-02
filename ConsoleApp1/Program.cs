7e86cc04fe01fe71dfba3eea7f37b3f2acf31125
Lina Bubon <ipz235_blv@student.ztu.edu.ua>
2025-02-02 18:10:28 +0200

    Консольний проєкт та gitignor

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.InputEncoding = Encoding.Unicode;
+        try
+        {
+            Console.WriteLine("Введіть число a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введіть число b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double sum = a + b;
+
+            Console.WriteLine($"\nСума чисел: {sum}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Помилка.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+        finally
+        {
+            Console.ReadKey();
+        }
+    }
+}
+
